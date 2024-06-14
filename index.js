10f02f32ddc65f0e4d623f95af4f3fbf9c9cd880
Waseem-Baig <waseem20032005@gmail.com>
2024-06-15 00:29:37 +0530

    One

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     console.log(url);
 
     // Send URL to the server to generate QR code
-    fetch("https://qr-code-generator-knsn.onrender.com/generateQR", {
+    fetch("http://localhost:3000/generateQR", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -15,35 +15,12 @@ $(document).ready(function () {
       .then((data) => {
         if (data.success) {
           console.log("QR code generated successfully");
-          const qrCodeUrl = `https://qr-code-generator-knsn.onrender.com${data.filePath}`;
-          downloadQRCode(qrCodeUrl);
+          // Trigger the download
+          window.location.href = "http://localhost:3000/downloadQR";
         } else {
           console.error("Error generating QR code");
         }
       })
       .catch((error) => console.error("Error:", error));
   });
-
-  function downloadQRCode(url) {
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "qr_code.png";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-
-    // Optionally, delete the QR code image after download
-    fetch("https://qr-code-generator-knsn.onrender.com/deleteQR", {
-      method: "POST",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          console.log("QR code deleted successfully");
-        } else {
-          console.error("Error deleting QR code");
-        }
-      })
-      .catch((error) => console.error("Error:", error));
-  }
 });
